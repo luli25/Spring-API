409a5ed4d20e0636216907cd586c15c8b4a82899
luli25 <40470742+luli25@users.noreply.github.com>
2021-02-17 11:13:18 -0300

    Added a class for exception management

--- src/main/java/com/example/demo/ClientService.java
+++ src/main/java/com/example/demo/ClientService.java
@@ -3,11 +3,17 @@ package com.example.demo;
 import com.example.demo.model.Client;
 import com.example.demo.model.dto.ClientDto;
 import com.example.demo.repositories.ClientRepository;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,11 +39,23 @@ public class ClientService {
     }
 
     public ClientDto findByName(String name) {
-        return entityToDto(clientRepo.findById(name).orElse(clientRepo.save(new Client(name))));
+        return entityToDto(clientRepo.findById(name).orElseThrow(NoSuchElementException::new));
     }
 
     public void addClient(Client client) {
         clientRepo.save(client);
     }
+
+    // This class handles any NoSuchElementException throw by findByName method
+    // and return a proper response
+    @ControllerAdvice
+    public static class NoSuchElementController {
+        @ExceptionHandler(value = NoSuchElementException.class)
+        public ResponseEntity<Object> exception(NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
 }
