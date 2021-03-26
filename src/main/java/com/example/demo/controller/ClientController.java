c85127d483fa7176cf49f80cd71661f7eb569bd8
luli25 <40470742+luli25@users.noreply.github.com>
2021-03-26 11:35:23 -0300

    Finishing last tests for the controller

--- src/main/java/com/example/demo/controller/ClientController.java
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -4,6 +4,7 @@ import com.example.demo.ClientService;
 import com.example.demo.model.Client;
 import com.example.demo.model.dto.ClientDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/test/groovy/com/example/demo/controller/ClientControllerTest.groovy
+++ src/test/groovy/com/example/demo/controller/ClientControllerTest.groovy
@@ -2,9 +2,11 @@ package com.example.demo.controller
 
 import com.example.demo.ClientService
 import com.example.demo.model.Client
+import com.example.demo.model.dto.ClientDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
@@ -12,6 +14,8 @@ import spock.lang.Specification
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [ClientController.class])
@@ -31,7 +35,29 @@ class ClientControllerTest extends Specification {
         mvc = MockMvcBuilders.standaloneSetup(controller).build()
     }
 
-    def "should add a new client to the database"() {
+    def "getAllClients method should return status 200 and a list of clients"() {
+        when:
+        controller.getAllClients() >> List.of(new Client("test"))
+
+        then:
+        mvc.perform(get("/Client").contentType("application/json"))
+                .andDo(print()).andExpect(status().isOk()).andReturn().response
+    }
+
+    def "FindByName should return status 200 and a client when a name is provided"() {
+        given:
+        controller.getAllClients() >> List.of(new Client("test"))
+
+        when:
+        controller.findByName() >> "test"
+
+        then:
+        mvc.perform(get("/Client/name").contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk()).andReturn().response
+    }
+
+    def "addClient method should add a new client to the database and return status 200"() {
         given:
         Client newClient = new Client()
         newClient.setName("test")
