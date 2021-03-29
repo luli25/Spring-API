86b62ced3a3c27aa55534444ed1e2fa698c199f3
luli25 <40470742+luli25@users.noreply.github.com>
2021-03-29 11:31:27 -0300

    Deleted unused imports

--- src/test/groovy/com/example/demo/controller/ClientControllerTest.groovy
+++ src/test/groovy/com/example/demo/controller/ClientControllerTest.groovy
@@ -2,20 +2,16 @@ package com.example.demo.controller
 
 import com.example.demo.ClientService
 import com.example.demo.model.Client
-import com.example.demo.model.dto.ClientDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [ClientController.class])
